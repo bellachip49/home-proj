38cb9a4c1c639987c33e0020d6e090bff5fcab30
bellachip49 <krystal.m.kwan@gmail.com>
2016-03-25 15:00:41 -0700

    make file

---
+++ README.md
@@ -0,0 +1,2 @@
+ssh pi@ompi.local
+these are the files for kk folder in the pi
---
+++ f1.py
@@ -0,0 +1,63 @@
+# Krystal Kwan
+# Friday March 25 2016
+# Press a button to make RGB LEDS turn red, blue, and green. 
+
+import random, time
+import RPi.GPIO as GPIO
+ 
+RUNNING = True
+GPIO.setmode(GPIO.BCM)
+red = 18
+green = 24
+blue = 23 
+ 
+GPIO.setup(red, GPIO.OUT)
+GPIO.setup(red, GPIO.OUT)
+GPIO.setup(green, GPIO.OUT)
+GPIO.setup(green, GPIO.OUT)
+GPIO.setup(blue, GPIO.OUT)
+GPIO.setup(blue, GPIO.OUT)
+
+Freq = 100 #Hz
+ 
+RED = GPIO.PWM(red, Freq)
+RED.start(0)
+GREEN = GPIO.PWM(green, Freq)
+GREEN.start(0)
+BLUE = GPIO.PWM(blue, Freq)
+BLUE.start(0)
+ 
+def color(R, G, B, on_time):
+    # Color brightness range is 0-100%
+    RED.ChangeDutyCycle(R)
+    GREEN.ChangeDutyCycle(G)
+    BLUE.ChangeDutyCycle(B)
+    time.sleep(on_time)
+ 
+    # Turn all LEDs off after on_time seconds
+    RED.ChangeDutyCycle(0)
+    GREEN.ChangeDutyCycle(0)
+    BLUE.ChangeDutyCycle(0)
+ 
+print("Light It Up!")
+print("Press CTRL + C to quit.\n")
+print(" R  G  B\n---------")
+ 
+# Main loop
+try:
+    while RUNNING:
+        for x in range(0,2):
+            for y in range(0,2):
+                for z in range(0,2):
+                    print (x,y,z)
+                    for i in range(0,101):
+                        color((x*i),(y*i),(z*i), .02)
+ 
+# If CTRL+C is pressed the main loop is broken
+except KeyboardInterrupt:
+    RUNNING = False
+    print "\Quitting"
+ 
+finally:
+    GPIO.cleanup()
+
