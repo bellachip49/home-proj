d917f58d5bef8c168b05f2b0cbe7b3d2aab79eec
Krystal Kwan <krystalkwan@krystal.attlocal.net>
2016-03-25 17:59:15 -0700

    edit file

--- f1.py
+++ f1.py
@@ -10,13 +10,16 @@ GPIO.setmode(GPIO.BCM)
 red = 18
 green = 24
 blue = 23 
+red1 = 16
+green1 = 12
+blue1 = 21 
  
 GPIO.setup(red, GPIO.OUT)
-GPIO.setup(red, GPIO.OUT)
+GPIO.setup(red1, GPIO.OUT)
 GPIO.setup(green, GPIO.OUT)
-GPIO.setup(green, GPIO.OUT)
+GPIO.setup(green1, GPIO.OUT)
 GPIO.setup(blue, GPIO.OUT)
-GPIO.setup(blue, GPIO.OUT)
+GPIO.setup(blue1, GPIO.OUT)
 
 Freq = 100 #Hz
  
@@ -26,18 +29,30 @@ GREEN = GPIO.PWM(green, Freq)
 GREEN.start(0)
 BLUE = GPIO.PWM(blue, Freq)
 BLUE.start(0)
+RED1 = GPIO.PWM(red1, Freq)
+RED1.start(0)
+GREEN1 = GPIO.PWM(green1, Freq)
+GREEN1.start(0)
+BLUE1 = GPIO.PWM(blue1, Freq)
+BLUE1.start(0)
  
 def color(R, G, B, on_time):
     # Color brightness range is 0-100%
     RED.ChangeDutyCycle(R)
     GREEN.ChangeDutyCycle(G)
     BLUE.ChangeDutyCycle(B)
+    RED1.ChangeDutyCycle(R)
+    GREEN1.ChangeDutyCycle(G)
+    BLUE1.ChangeDutyCycle(B)
     time.sleep(on_time)
  
     # Turn all LEDs off after on_time seconds
     RED.ChangeDutyCycle(0)
     GREEN.ChangeDutyCycle(0)
     BLUE.ChangeDutyCycle(0)
+    RED1.ChangeDutyCycle(0)
+    GREEN1.ChangeDutyCycle(0)
+    BLUE1.ChangeDutyCycle(0)
  
 print("Light It Up!")
 print("Press CTRL + C to quit.\n")
@@ -56,7 +71,7 @@ try:
 # If CTRL+C is pressed the main loop is broken
 except KeyboardInterrupt:
     RUNNING = False
-    print "\Quitting"
+    print ("\Quitting")
  
 finally:
     GPIO.cleanup()
