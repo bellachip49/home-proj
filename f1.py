71f3173362eddf851bb1471ad0fac8523a5d4fc1
Bella Chip <krystal.m.kwan@gmail.com>
2016-04-10 13:34:56 -0700

    edit file

--- f1.py
+++ f1.py
@@ -10,11 +10,18 @@ GPIO.setwarnings(False)
 red = 18
 green = 24
 blue = 23 
+red1 = 12
+green1 = 16
+blue1 = 20
  
 GPIO.setup(red, GPIO.OUT)
 GPIO.setup(green, GPIO.OUT)
 GPIO.setup(blue, GPIO.OUT)
 GPIO.setup(25, GPIO.IN, pull_up_down = GPIO.PUD_UP)
+GPIO.setup(21, GPIO.IN, pull_up_down = GPIO.PUD_UP)
+GPIO.setup(red1, GPIO.OUT)
+GPIO.setup(green1, GPIO.OUT)
+GPIO.setup(blue1, GPIO.OUT)
 
 Freq = 100 
  
@@ -24,30 +31,57 @@ GREEN = GPIO.PWM(green, Freq)
 GREEN.start(0)
 BLUE = GPIO.PWM(blue, Freq)
 BLUE.start(0)
+RED1 = GPIO.PWM(red1, Freq)
+RED1.start(0)
+GREEN1 = GPIO.PWM(green1, Freq)
+GREEN1.start(0)
+BLUE1 = GPIO.PWM(blue1, Freq)
+BLUE1.start(0)
  
 def color(R, G, B):
     RED.ChangeDutyCycle(R)
     GREEN.ChangeDutyCycle(G)
     BLUE.ChangeDutyCycle(B)
- 
-turn = 0
-while True:
-    if(GPIO.input(25) == 0):
+
+def color1(R1, G1, B1):
+    RED1.ChangeDutyCycle(R1)
+    GREEN1.ChangeDutyCycle(G1)
+    BLUE1.ChangeDutyCycle(B1)
+    
+def button1(turn):
         turn +=1
         if turn == 1:
           color(100,0,0)
-          GREEN.ChangeDutyCycle(0)
-          BLUE.ChangeDutyCycle(0)
+          time.sleep(.2)
         elif turn == 2:
           color(0,100,0)
-          BLUE.ChangeDutyCycle(0)
-          RED.ChangeDutyCycle(0)
+          time.sleep(.2)
         elif turn == 3:
           color(0,0,100)
-          GREEN.ChangeDutyCycle(0)
-          RED.ChangeDutyCycle(0)
+          time.sleep(.2)
         else:
           turn = 0
+
+def button2(turn):
+        turn +=1
+        if turn == 1:
+          color(100,0,0)
+          time.sleep(.2)
+        elif turn == 2:
+          color(0,100,0)
+          time.sleep(.2)
+        elif turn == 3:
+          color(0,0,100)
+          time.sleep(.2)
+        else:
+          turn = 0
+
+turn = 0
+while True:
+    if(GPIO.input(25) == 0):
+        button1(turn)
+    if(GPIO.input(21) == 0):
+        button2(turn)
 
 GPIO.cleanup()
 
