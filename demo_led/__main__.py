ff3874933d27dc578a45f120882daecbbc40e589
Bella Chip <krystal.m.kwan@gmail.com>
2016-05-01 14:50:55 -0700

    make file

--- demo_led/__main__.py
+++ demo_led/__main__.py
@@ -1,10 +1,10 @@
 # Krystal Kwan
 # Friday March 25 2016
-# Press a button to make RGB LEDS turn red, blue, and green. 
+# Press a button to make RGB LEDS turn red, blue, and green.
 
 import random, time
 import RPi.GPIO as GPIO
- 
+
 def color_test(LED):
    LED['turn'] += 1
    if LED['turn'] == 1:
@@ -24,17 +24,16 @@ def color_test(LED):
        time.sleep(.2)
    else:
        LED['turn'] = 0
-      
-def main():
+
+ def main():
     GPIO.setmode(GPIO.BCM)
     GPIO.setwarnings(False)
     red = 18
     green = 24
-    blue = 23 
+    blue = 23
     red1 = 12
     green1 = 16
     blue1 = 20
-     
     GPIO.setup(red, GPIO.OUT)
     GPIO.setup(green, GPIO.OUT)
     GPIO.setup(blue, GPIO.OUT)
@@ -43,33 +42,33 @@ def main():
     GPIO.setup(red1, GPIO.OUT)
     GPIO.setup(green1, GPIO.OUT)
     GPIO.setup(blue1, GPIO.OUT)
-    
-    Freq = 100 
-     
+    Freq = 100
     RED = GPIO.PWM(red, Freq)
     RED.start(0)
     GREEN = GPIO.PWM(green, Freq)
     GREEN.start(0)
     BLUE = GPIO.PWM(blue, Freq)
     BLUE.start(0)
-    RED1 = GPIO.PWM(red1, Freq)
-    RED1.start(0)
-    GREEN1 = GPIO.PWM(green1, Freq)
-    GREEN1.start(0)
-    BLUE1 = GPIO.PWM(blue1, Freq)
-    BLUE1.start(0)
-    
-    led0 = { 'RED' :RED, 'GREEN': GREEN, 'BLUE': BLUE, 'turn': 0 }
-    led1 = { 'RED' :RED1, 'GREEN': GREEN1, 'BLUE': BLUE1, 'turn': 0 }
-     
-    while True:
-        if (GPIO.input(25) == 0):
-            color_test(led0)
-        if (GPIO.input(21) == 0):
-           	color_test(led1)
-    
-    GPIO.cleanup()
+#     RED1 = GPIO.PWM(red1, Freq)
+#     RED1.start(0)
+#     GREEN1 = GPIO.PWM(green1, Freq)
+#     GREEN1.start(0)
+#     BLUE1 = GPIO.PWM(blue1, Freq)
+#     BLUE1.start(0)
+#
+#     led0 = { 'RED' :RED, 'GREEN': GREEN, 'BLUE': BLUE, 'turn': 0 }
+#     led1 = { 'RED' :RED1, 'GREEN': GREEN1, 'BLUE': BLUE1, 'turn': 0 }
+#
+#     while True:
+#         if (GPIO.input(25) == 0):
+#             color_test(led0)
+#         if (GPIO.input(21) == 0):
+#            	color_test(led1)
+#
+#     GPIO.cleanup()
 
+def main():
+    print("hello world")
 
 if __name__ == '__main__':
   main()
