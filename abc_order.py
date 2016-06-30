8a2c0e0d744e68c459760a84dd940aa7996ce3ab
Bella Chip <krystal.m.kwan@gmail.com>
2016-06-30 16:20:23 -0700

    make strings in alphabetical order

---
+++ abc_order.py
@@ -0,0 +1,17 @@
+#organizes things in order
+
+nxt = 1
+wdlist = []
+print("Press ^C when done typing words.")
+while True:
+    try:
+        print (("Word #%i:") % (nxt))
+        word = raw_input("> ")
+        wdlist.append(word)
+        nxt += 1
+    except KeyboardInterrupt:
+        break
+print
+for i in sorted(wdlist):
+    print i
+    nxt = raw_input("Press ENTER to quit: ")
